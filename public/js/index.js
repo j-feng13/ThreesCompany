faa6b83d4c1184d7f31cbb0e2b9073a404ae9993
Kyler Berry <kyler.berry@zumba.com>
2016-06-18 03:37:36 -0400

    add first markup submission and code editor

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,9 @@
-$('document').on('ready', function() {
+$(document).ready(function() {
+	var editor = ace.edit("editor");
+	var textarea = $('#textarea');
+
+	editor.on('change', function() {
+		console.log('change');
+		textarea.val(editor.getSession().getValue());
+	});
 });
